afd4aa30982bc0b383bb825df8dbef7f34643179
Magdalen Thot <43592505+magdalent@users.noreply.github.com>
2021-04-28 18:38:25 -0600

    Add files via upload

---
+++ Sketches/circles/sketch.js
@@ -0,0 +1,156 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0,0,0);
+
+
+//GLOBE:  
+noStroke()
+fill(0,0,225);
+ellipse(200, 200, 300, 300);
+
+noStroke()
+fill(233,3,33,100);
+ellipse(200, 200, 290, 290);
+
+noStroke()
+fill(223,33,33,90);
+ellipse(200, 200, 280, 280);
+
+noStroke()
+fill(223,33,33,90);
+ellipse(200, 200, 270, 270);
+  
+noStroke()
+fill(23,23,3,80);
+ellipse(200, 200, 260, 260);
+  
+  noStroke()
+fill(23,23,3,80);
+ellipse(200, 200, 250, 250);
+  
+  
+noStroke()
+fill(23,23,3,80);
+ellipse(200, 200, 240, 240);
+  
+  
+noStroke()
+fill(23,2,235,90);
+ellipse(200, 200, 230, 230);
+  
+  noStroke()
+fill(90,20,25,90);
+ellipse(200, 200, 220, 220);
+  
+  
+  noStroke()
+fill(59,33,205,100);
+ellipse(200, 200, 210, 210);
+  
+  
+  noStroke()
+fill(20,222,100,120);
+ellipse(200, 200, 200, 200);
+  
+  
+noStroke()
+fill(30,120,0,100);
+ellipse(200, 200, 190, 190);
+  
+noStroke()
+fill(255,10,5,90);
+ellipse(200, 200, 180, 180);
+  
+  noStroke()
+fill(0,0,255,95);
+ellipse(200, 200, 170, 170);
+  
+  noStroke()
+fill(30,120,255,100);
+ellipse(200, 200, 160, 160);
+  
+  
+  noStroke()
+fill(30,120,255,100);
+ellipse(200, 200, 150, 150);
+  
+  
+  noStroke()
+fill(30,120,255,100);
+ellipse(200, 200, 140, 140);
+  
+  
+  noStroke()
+fill(30,120,255,100);
+ellipse(200, 200, 130, 130);
+  
+  noStroke()
+fill(30,120,255,100);
+ellipse(200, 200, 120, 120);
+  
+  
+  noStroke()
+fill(30,120,255,100);
+ellipse(200, 200, 130, 130);
+  
+  
+noStroke()
+fill(40,20,255,100);
+ellipse(200, 200, 120, 120);
+  
+noStroke()
+fill(40,20,255,100);
+ellipse(200, 200, 110, 110);
+
+noStroke()
+fill(40,20,255,100);
+ellipse(200, 200, 100, 100);
+
+noStroke()
+fill(40,20,255,100);
+ellipse(200, 200, 90, 90);
+
+noStroke()
+fill(254,91,91,80);
+ellipse(200, 200, 80, 80);
+  
+noStroke()
+fill(254,91,91,80);
+ellipse(200, 200, 70, 70);
+  
+noStroke()
+fill(254,91,91,80);
+ellipse(200, 200, 60, 60);
+  
+  noStroke()
+fill(254,91,91,80);
+ellipse(200, 200, 50, 50);
+  
+  noStroke()
+fill(254,91,91,80);
+ellipse(200, 200, 40, 40);
+  
+noStroke()
+fill(255,226,1,80);
+ellipse(200, 200, 30, 30);
+
+noStroke()
+fill(255,226,1,80);
+ellipse(200, 200, 20, 20);
+
+noStroke()
+fill(255,226,1,80);
+ellipse(200, 200, 10, 10);
+// END
+
+
+
+
+
+
+
+  
+}
